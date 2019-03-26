e6c1ff434d8ff4521197f717256f9340310383a7
Felix Dietze <felix.dietze@rwth-aachen.de>
2019-03-26 18:16:43 +0100

    Add InterleavedArrayDouble

---
+++ flatland/src/main/scala/InterleavedArrayDouble.scala
@@ -0,0 +1,51 @@
+package flatland
+
+import scala.reflect.ClassTag
+
+object InterleavedArrayDouble {
+  @inline def create(n:Int): InterleavedArrayDouble = new InterleavedArrayDouble(new Array[Double](n*2))
+
+  implicit def toIndexdSeq(interleaved: InterleavedArrayDouble): IndexedSeq[(Double,Double)] = new IndexedSeq[(Double,Double)] {
+    def apply(idx: Int): (Double,Double) = (interleaved.a(idx), interleaved.b(idx))
+    def length: Int = interleaved.elementCount
+  }
+}
+
+@inline final class InterleavedArrayDouble(val interleaved:Array[Double]) {
+  @inline def a(i:Int): Double = interleaved(i*2)
+  @inline def b(i:Int): Double = interleaved(i*2+1)
+  @inline def updatea(i:Int, value:Int): Unit = interleaved(i*2) = value
+  @inline def updateb(i:Int, value:Int): Unit = interleaved(i*2+1) = value
+
+  @inline def x(i:Int): Double = a(i)
+  @inline def y(i:Int): Double = b(i)
+  @inline def updatex(i:Int, value:Int): Unit = updatea(i,value)
+  @inline def updatey(i:Int, value:Int): Unit = updateb(i,value)
+
+  @inline def left(i:Int): Double = a(i)
+  @inline def right(i:Int): Double = b(i)
+  @inline def updateLeft(i:Int, value:Int): Unit = updatea(i,value)
+  @inline def updateRight(i:Int, value:Int): Unit = updateb(i,value)
+
+  @inline def elementCount:Int = interleaved.length / 2
+
+  @inline def foreachTwoElements(f: (Double,Double) => Unit): Unit = {
+    val n = elementCount
+    var i = 0
+
+    while(i < n ) {
+      f(a(i), b(i))
+      i += 1
+    }
+  }
+  @inline def foreachIndexAndTwoElements(f: (Int,Double,Double) => Unit): Unit = {
+    val n = elementCount
+    var i = 0
+
+    while(i < n ) {
+      f(i, a(i), b(i))
+      i += 1
+    }
+  }
+}
+
