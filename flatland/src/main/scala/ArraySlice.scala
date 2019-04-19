b0b3e3e2d603fa0857d3222cce244f47d6855f31
Felix Dietze <felix.dietze@rwth-aachen.de>
2019-04-20 00:12:33 +0200

    ArraySlice: public members

--- flatland/src/main/scala/ArraySlice.scala
+++ flatland/src/main/scala/ArraySlice.scala
@@ -2,7 +2,11 @@ package flatland
 
 import scala.collection.mutable
 
-@inline final class ArraySliceInt(array: Array[Int], start: Int, val length: Int) extends IndexedSeq[Int] {
+@inline final class ArraySliceInt(val array: Array[Int], val start: Int, val length: Int) extends IndexedSeq[Int] {
+  @inline override def apply(idx: Int): Int = array(start + idx)
+  @inline override def isEmpty = length == 0
+  @inline override def slice(from: Int, until: Int): ArraySliceInt = new ArraySliceInt(array, start + from, until - from)
+
   override protected[this] def newBuilder: mutable.Builder[Int, IndexedSeq[Int]] = new mutable.Builder[Int, IndexedSeq[Int]] {
     val self = new mutable.ArrayBuilder.ofInt
     override def +=(elem: Int): this.type = { self += elem; this }
@@ -10,10 +14,7 @@ import scala.collection.mutable
     override def result(): IndexedSeq[Int] = self.result()
     override def sizeHint(size: Int): Unit = self.sizeHint(size)
   }
-
-  @inline override def apply(idx: Int): Int = array(start + idx)
   override def iterator: Iterator[Int] = array.iterator.slice(start, start + length)
-  @inline override def slice(from: Int, until: Int): ArraySliceInt = new ArraySliceInt(array, start + from, until - from)
 }
 
 object ArraySliceInt {
