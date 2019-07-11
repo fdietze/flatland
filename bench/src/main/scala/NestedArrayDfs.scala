024800bb1c89b4fe39d7af88bf86232b4aef71e4
Felix Dietze <felix.dietze@rwth-aachen.de>
2019-07-11 12:58:30 -0400

    indirect dfs

--- bench/src/main/scala/NestedArrayDfs.scala
+++ bench/src/main/scala/NestedArrayDfs.scala
@@ -9,7 +9,7 @@ import collection.mutable
 // to run this benchmark in JS:
 // ;set mainClass in Compile := Some("flatland.bench.NestedArrayDfs"); run
 object NestedArrayDfs {
-  def generateLatticeGraph(size: Int): NestedArrayInt = {
+  def generateLatticeLookup(size: Int): NestedArrayInt = {
     val n = Math.sqrt(size).floor.toInt
     NestedArrayInt(Array.tabulate(size){ i =>
       Array(i - 1).filter(x => x >= (i / n) * n) ++
@@ -18,55 +18,96 @@ object NestedArrayDfs {
         Array(i + n).filter(x => x < size)
     })
   }
-  // def generateIndirectLatticeGraph(size: Int): NestedArrayInt = {
-  //   // indirect in the sense that the nestedArray stores edge indices (instead of vertex indices)
-  //   // it is then mapped to lookup the node index in the edge array
-  //   val successors = generateLatticeGraph(size)
-  //   val edgesBuilder = new mutable.ArrayBuilder.ofRef[(Int,Int)]
-  //   val incidenceBuilder = Array.fill(successors.size)(new mutable.ArrayBuilder.ofInt)
-  //   var edgeIdx = 0
-  //   successors.foreachIndexAndSlice { (sourceIdx, targets) =>
-  //     targets.foreachElement { targetIdx =>
-  //       edgesBuilder += (sourceIdx -> targetIdx)
-  //       incidenceBuilder(sourceIdx) += edgeIdx
+   
+  def generateLatticeGraph(size: Int): (NestedArrayInt, InterleavedArrayInt, NestedArrayInt) = {
+    // indirect in the sense that the nestedArray stores edge indices (instead of vertex indices)
+    // it is then mapped to lookup the node index in the edge array
+    val successors = generateLatticeLookup(size)
+    val edgesBuilder = new mutable.ArrayBuilder.ofRef[(Int, Int)]
+    val incidenceBuilder = Array.fill(successors.size)(new mutable.ArrayBuilder.ofInt)
+    var edgeIdx = 0
+    successors.foreachIndexAndSlice { (sourceIdx, targets) =>
+      targets.foreachElement { targetIdx =>
+        edgesBuilder += (sourceIdx -> targetIdx)
+        incidenceBuilder(sourceIdx) += edgeIdx
 
-  //       edgeIdx += 1
-  //     }
-  //   }
-  //   val edges = InterleavedArrayInt(edgesBuilder.result)
-  //   val incidenceLookup = NestedArrayInt(incidenceBuilder).viewMapInt(edgeIdx => edges.right(edgeIdx))
+        edgeIdx += 1
+      }
+    }
+    val edges = InterleavedArrayInt(edgesBuilder.result)
+    val incidenceLookup = NestedArrayInt(incidenceBuilder)
+    // val indirectIncidenceLookup = NestedArrayInt(incidenceBuilder).viewMapInt(edgeIdx => edges.right(edgeIdx))
 
+    // if (successors.map(_.toList) != incidenceLookup.map(_.toList)) throw new Exception("Not Equal!")
 
-  //   if(successors.map(_.toList) != incidenceLookup.map(_.toList)) throw new Exception("Not Equal!")
-
-  //   incidenceLookup
-  // }
+    (successors, edges, incidenceLookup)
+  }
 
   def main(args: Array[String]): Unit = {
     assert(false, "assertions enabled")
     val comparison = Comparison("NestedArrayInt", Seq(
-      BenchmarkImmutableInit[NestedArrayInt](
+      BenchmarkImmutableInit[(NestedArrayInt, InterleavedArrayInt, NestedArrayInt)](
         "NestedArrayIntValues",
         size => generateLatticeGraph(size),
         {
-          nestedArray =>
+          case (lookup, _, _) =>
             // println("dfs1 start")
-            nestedArray.depthFirstSearchToArray(0)
-            // println("dfs1 end")
+            lookup.depthFirstSearchToArray(0)
+          // println("dfs1 end")
         }
       ),
-      // BenchmarkImmutableInit[NestedArrayInt](
-      //   "NestedArrayIntMapped",
-      //   size => generateIndirectLatticeGraph(size),
+      // BenchmarkImmutableInit[(NestedArrayInt, InterleavedArrayInt, NestedArrayInt)](
+      //   "NestedArrayInt generic dfs",
+      //   size => generateLatticeGraph(size),
       //   {
-      //     nestedArray =>
-      //       // println("dfs2 start")
-      //       nestedArray.depthFirstSearchToArray(0)
-      //       // println("dfs2 end")
+      //     case (lookup, _, _) =>
+      //       val builder = new mutable.ArrayBuilder.ofInt
+
+      //       flatland.depthFirstSearchGeneric(
+      //         vertexCount = lookup.length,
+      //         foreachSuccessor = (idx, f) => lookup.foreachElement(idx)(f),
+      //         init = (stack, _) => stack.push(0),
+      //         processVertex = builder += _,
+      //         loopConditionGuard = condition => condition(),
+      //         advanceGuard = (result: scala.collection.mutable.ArrayBuilder.ofInt, advance: () => Unit) => advance(),
+      //         enqueueGuard = (elem, enqueue) => enqueue()
+      //       )
+
+      //       builder.result()
       //   }
       // ),
+      BenchmarkImmutableInit[(NestedArrayInt, InterleavedArrayInt, NestedArrayInt)](
+        "NestedArrayInt indirect generic dfs",
+        size => generateLatticeGraph(size),
+        {
+          case (_, edges, lookup) =>
+            val builder = new mutable.ArrayBuilder.ofInt
+
+            flatland.depthFirstSearchGeneric(
+              vertexCount = lookup.length,
+              foreachSuccessor = (idx, f) => lookup.foreachElement(idx)(edgeIdx => f(edges.right(edgeIdx))),
+              init = (stack, _) => stack.push(0),
+              processVertex = builder += _,
+              loopConditionGuard = condition => condition(),
+              advanceGuard = (result: scala.collection.mutable.ArrayBuilder.ofInt, advance: () => Unit) => advance(),
+              enqueueGuard = (elem, enqueue) => enqueue()
+            )
+
+            builder.result()
+        }
+      ),
+    // BenchmarkImmutableInit[NestedArrayInt](
+    //   "NestedArrayIntMapped",
+    //   size => generateIndirectLatticeGraph(size),
+    //   {
+    //     nestedArray =>
+    //       // println("dfs2 start")
+    //       nestedArray.depthFirstSearchToArray(0)
+    //       // println("dfs2 end")
+    //   }
+    // ),
     ))
-    runComparison(comparison, List(100, 1000, 10000), 60 seconds)
+    runComparison(comparison, List(100, 1000, 10000, 1000000), 60 seconds)
 
     ()
   }
